13d230950525431dff4632a8f1ab7abd281b61d9
Koundinya Vajjha <koundinya.vajjha@gmail.com>
2018-01-10 05:01:44 -0500

    Revert "The kernel of a monoid is a subgroup."
    This reverts commit a8f6ce0ac13348c83b626c5ecd2d2385d1826a26.

--- UniMath/Algebra/Isom_thms.v
+++ UniMath/Algebra/Isom_thms.v
@@ -4,7 +4,7 @@
 Require Export UniMath.Algebra.Monoids_and_Groups.
 Require Export UniMath.Algebra.Rigs_and_Rings.
 
-(* We are trying to prove the Isomorphism theorems in UniMath. Let us start slow by proving it for groups. *)
+(*We are trying to prove the Isomorphism theorems in UniMath. Let us start slow by proving it for groups. *)
 
 (*
 
@@ -16,40 +16,38 @@ The first isomorphism theorem for groups says that if G, H are groups and  ϕ : 
 *)
 
 Open Scope multmonoid.
+Parameter X : gr.
+Check unel X.
+Check issubgr.
+Print hsubtype.
+
+Lemma unelmonoidfun_ismonoidfun (X Y : monoid) : ismonoidfun (λ x : X, (unel Y)).
+Proof.
+  use mk_ismonoidfun.
+  - use mk_isbinopfun. intros x x'. use pathsinv0. use lunax.
+  - use idpath.
+Qed.
+
+Compute S 1.
+
 
 
 Definition grkerhsubtype {G H : gr} (p : monoidfun G H) : hsubtype G :=  (λ g:G, ∥ hfiber p (unel H) ∥).
-Definition grimghsubtype {G H : gr} (p : monoidfun G H) : hsubtype H :=  (λ g:H, ∥ hfiber p (g) ∥).
 Parameter G : gr.
+
+Print issubsetwithbinop.
+Print issubgrpair.
 Parameter H : gr.
-Parameter g : G.
 Parameter p : monoidfun G H.
-Parameter a : grkerhsubtype p.
-Check pr2 a.
+Check grkerhsubtype.
+Check pr1.
 
-
-(* The kernel of a monoidfun is a subgroup *)
 
 Lemma issubgrgrker (G H : gr) (p : monoidfun G H) : issubgr (grkerhsubtype p).
 Proof.
   use issubgrpair.
-  (* Now we have to prove that it is a submonoid and preserves inverses *)
-
   - use issubmonoidpair.
- (*Have to show it has an associative binop and identity *)
     + intros a a'.
-      unfold grkerhsubtype.
-      use (hinhuniv _ (pr2 a)). (* pr2 a is wit of type ∥hfiber p 1∥ *)
-      intros.
-      use hinhpr. exact X.
-    + unfold grkerhsubtype.
-      use hinhpr.
-      unfold hfiber.
-      exists (unel G).
-      use ismonoidfununel.
-      exact (pr2 p).
-  (* Have to show it preserves inverses.*)
-  - intros. unfold grkerhsubtype in X. unfold grkerhsubtype. exact X.
-Defined.
-
-Lemma issubgrimg (G H : gr) (p : monoidfun G H) :
+      use (hinhuniv _ (pr2 a)). intros X0.
+      use (hinhuniv _ (pr2 a')). intros X1.
+      use hinhpr. exact X0.
