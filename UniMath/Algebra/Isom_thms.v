c82fce21ce0907e43ca6d35429aaa80d9d0b2fee
Koundinya Vajjha <koundinya.vajjha@gmail.com>
2018-01-21 11:22:19 -0500

    Finished proving that the image of a monoidfun is a subgroup of the codomain.

--- UniMath/Algebra/Isom_thms.v
+++ UniMath/Algebra/Isom_thms.v
@@ -16,9 +16,8 @@ The first isomorphism theorem for groups says that if G, H are groups and  ϕ : 
 
 Open Scope multmonoid.
 
-
 Definition grkerhsubtype {G H : gr} (p : monoidfun G H) : hsubtype G :=  (λ g:G, ∥ hfiber p (unel H) ∥).
-Definition grimghsubtype {G H : gr} (p : monoidfun G H) : hsubtype H :=  (λ g:H, ∥ hfiber p (g) ∥).
+Definition grimghsubtype {G H : gr} (p : monoidfun G H) : hsubtype H :=  (λ h:H, ∃ g:G, p(g) = h ).
 Parameter G : gr.
 Parameter H : gr.
 Parameter g : G.
@@ -51,5 +50,34 @@ Proof.
   - intros. unfold grkerhsubtype in X. unfold grkerhsubtype. exact X.
 Defined.
 
-Lemma issubgrimg (G H : gr) (p : monoidfun G H) :
-
+(* The image of a monoidfun is a subgroup *)
+Lemma issubgrimg (G H : gr) (p : monoidfun G H) : issubgr (grimghsubtype p).
+Proof.
+  use issubgrpair.
+  (* Now we have to prove that it is a submonoid and that it preserves inverses *)
+  - use issubmonoidpair.
+    (* Have to show it has an associative binop and identity *)
+    + intros a a'. unfold grimghsubtype.
+      use (hinhuniv _ (pr2 a)). intros ae.
+      use (hinhuniv _ (pr2 a')). intros ae'.
+      set (one := pr2 ae). simpl in one.
+      set (two := pr2 ae'). simpl in two.
+      set (prod := pr1 ae * pr1 ae').
+      use hinhpr.
+      exists prod. unfold prod.
+      use (pathscomp0 (binopfunisbinopfun p (pr1 ae) (pr1 ae'))).
+      use two_arg_paths. exact one. exact two.
+    + (* Preserves identity *)
+      unfold grimghsubtype.
+      use hinhpr. exists (unel G).
+      exact (dirprod_pr2(pr2 p)).
+  - (* Preserves inverses *)
+    intros x X.
+    unfold grimghsubtype in X. unfold grimghsubtype.
+    use (hinhuniv _ X). intro xe.
+    use hinhpr. exists (grinv G (pr1 xe)).
+    unfold monoidfun in p. unfold ismonoidfun in p.
+    use (pathscomp0 _ (maponpaths (λ bb : H, (grinv H bb)) (pr2 xe))).
+    (* Coq now asks us to fill the hole. No pun intended. *)
+    use monoidfuninvtoinv.
+Defined.
